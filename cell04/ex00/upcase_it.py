96bf0804ac1db825667da9131f4d3fc336407c75
nopbys <pob_ronaldo@hotmail.com>
2025-09-16 03:45:49 +0700

    Add files via upload

---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,2 @@
+word = str(input("Give me a Word: "))
+print(word.upper())
---
+++ cell04/ex03/float.py
@@ -0,0 +1,5 @@
+num = float(input("Give me a number: "))
+if num.is_integer() == True:
+    print("The number is an integer.")
+else:
+    print("The number is a decimal.")
